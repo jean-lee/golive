42b6c8d4fbeba3d7880ae7f01b1fcd43aa2862d8
jean-lee <1486075310@qq.com>
2020-03-09 14:06:22 +0800

    embed set

--- src/views/live/ali_player/ali_player.vue
+++ src/views/live/ali_player/ali_player.vue
@@ -25,86 +25,86 @@ import CommentDanmu from '@/views/live/barrage/comment_danmu.vue';
 export default class AliPlayer extends Vue {
   /* ------------------------ INPUT & OUTPUT ------------------------ */
   // 阿里播放器引用地址
-  @Prop({type: String, default: 'http://g.alicdn.com/de/prismplayer/2.8.7/aliplayer-min.js'}) private aliplayerSdkPath!: string;
+  @Prop({type: String, default: 'http://g.alicdn.com/de/prismplayer/2.8.7/aliplayer-min.js'}) public aliplayerSdkPath!: string;
   // 播放器自定义样式style '100%' | '100px' 宽高比规则：  w:h = 16:9
-  @Prop({type: Object, default() { return {width: '924px', height: '520px'}; }}) private playStyle!: object;
+  @Prop({type: Object, default() { return {width: '924px', height: '520px'}; }}) public playStyle!: object;
 
-  @Prop({type: Boolean, default: false}) private autoplay!: boolean;
+  @Prop({type: Boolean, default: false}) public autoplay!: boolean;
    // 播放内容是否为直播，直播时会禁止用户拖动进度条
-  @Prop({type: Boolean, default: true}) private isLive!: boolean;
+  @Prop({type: Boolean, default: true}) public isLive!: boolean;
 
   // 播放器宽度 | 高度
-  @Prop({type: String, default: '100%'}) private width!: string;
-  @Prop({type: String, default: '100%'}) private height!: string;
+  @Prop({type: String, default: '100%'}) public width!: string;
+  @Prop({type: String, default: '100%'}) public height!: string;
   // controlBarVisibility -> hover | click | always
-  @Prop({type: String, default: 'always'}) private controlBarVisibility!: string;
+  @Prop({type: String, default: 'always'}) public controlBarVisibility!: string;
   // 指定使用H5播放器
-  @Prop({type: Boolean, default: false}) private useH5Prism!: boolean;
+  @Prop({type: Boolean, default: false}) public useH5Prism!: boolean;
   // 指定使用Flash播放器
-  @Prop({type: Boolean, default: false}) private useFlashPrism!: boolean;
+  @Prop({type: Boolean, default: false}) public useFlashPrism!: boolean;
   // H5是否内置播放
-  @Prop({type: Boolean, default: true}) private playsinline!: boolean;
+  @Prop({type: Boolean, default: true}) public playsinline!: boolean;
   // 指定播放地址格式，只有使用vid+plauth播放方式时支持可选值为'mp4'和'm3u8',默认为'mp4'
-  @Prop({type: String, default: 'm3u8'}) private format!: string;
+  @Prop({type: String, default: 'm3u8'}) public format!: string;
   // 显示缓冲进度
-  @Prop({type: Boolean, default: true}) private showBuffer!: boolean;
+  @Prop({type: Boolean, default: true}) public showBuffer!: boolean;
   // 快照
-  @Prop({type: Boolean, default: true}) private snapshot!: boolean;
+  @Prop({type: Boolean, default: true}) public snapshot!: boolean;
   // 媒体转码服务的媒体Id
-  @Prop({type: String, default: ''}) private vid!: string;
-  @Prop({type: String, default: ''}) private playauth!: string;
+  @Prop({type: String, default: ''}) public vid!: string;
+  @Prop({type: String, default: ''}) public playauth!: string;
   // 视频播放地址url：1、单独url；2、默认状态，表示使用“vi+playauth3、source播放方式优先级最高
-  @Prop({type: String, default: ''}) private source!: string;
+  @Prop({type: String, default: ''}) public source!: string;
   // 播放器默认封面图片，请填写正确的图片url地址Flash播放器封面也需要开启允许跨域访问
   // 自动加载与自动播放为false时生效显示
-  @Prop({type: String, default: ''}) private cover!: string;
+  @Prop({type: String, default: ''}) public cover!: string;
 
   // 声明启用同层H5播放器，启用时设置的值为'h5'具体参考同层播放 默认'auto'
-  @Prop({type: String, default: 'h5'}) private x5Type!: string;
+  @Prop({type: String, default: 'h5'}) public x5Type!: string;
   // 声明视频播放时是否进入到TBS的全屏模式，默认为false具体参考同层播放
-  @Prop({type: Boolean, default: false}) private x5Fullscreen!: boolean;
+  @Prop({type: Boolean, default: false}) public x5Fullscreen!: boolean;
   // 声明视频播在界面上的位置，默认为"center" 可选值为：'top'，'center' 具体参考同层播放
-  @Prop({type: String, default: 'top'}) private x5VideoPosition!: string;
+  @Prop({type: String, default: 'top'}) public x5VideoPosition!: string;
   // 声明TBS播放器支持的方向，可选值：landscape:横屏） portraint:竖屏 landscape
-  @Prop({type: Number, default: 2}) private x5Orientation!: number;
+  @Prop({type: Number, default: 2}) public x5Orientation!: number;
 
-  // @Prop({type: Number, default: 0}) private autoPlayNumber!: number;
+  // @Prop({type: Number, default: 0}) public autoPlayNumber!: number;
   // 延迟播放时间，单位为秒具体参考延迟播放
-  @Prop({type: Number, default: 0}) private autoPlayDelay!: number;
+  @Prop({type: Number, default: 0}) public autoPlayDelay!: number;
   // 延迟播放提示文本具体参考延迟播放
-  @Prop({type: String, default: 'waiting'}) private autoPlayDelayDisplayText!: string;
+  @Prop({type: String, default: 'waiting'}) public autoPlayDelayDisplayText!: string;
 
 
   // 重新加载视频
-  // @Prop({type: Boolean, default: false}) private isReload!: boolean;
+  // @Prop({type: Boolean, default: false}) public isReload!: boolean;
   // 显示弹幕
-  @Prop({type: Boolean, default: false}) private showDanmu!: boolean;
+  @Prop({type: Boolean, default: false}) public showDanmu!: boolean;
 
-  @Emit('ready') private readyEmit() {}
-  @Emit('play') private playEmit() {}
-  @Emit('pause') private pauseEmit() {}
-  @Emit('ended') private endedEmit() {}
-  @Emit('live-streamstop') private liveStreamStopEmit() {}
-  @Emit('m3u8retry') private m3u8RetryEmit() {}
-  @Emit('hidebar') private hideBarEmit() {}
-  @Emit('waiting') private waitingEmit() {}
-  @Emit('snapshoted') private snapshotedEmit() {}
+  @Emit('ready') public readyEmit() {}
+  @Emit('play') public playEmit() {}
+  @Emit('pause') public pauseEmit() {}
+  @Emit('ended') public endedEmit() {}
+  @Emit('live-streamstop') public liveStreamStopEmit() {}
+  @Emit('m3u8retry') public m3u8RetryEmit() {}
+  @Emit('hidebar') public hideBarEmit() {}
+  @Emit('waiting') public waitingEmit() {}
+  @Emit('snapshoted') public snapshotedEmit() {}
 
   /* ------------------------ VUEX (vuex getter & vuex action) ------------------------ */
 
   /* ------------------------ LIFECYCLE HOOKS (created & mounted & ...) ------------------------ */
-  // private created() {
+  // public created() {
   //   this.get_window_aliplayer();
   // }
-  private mounted() {
+  public mounted() {
     this.get_window_aliplayer();
   }
   /* ------------------------ COMPONENT STATE (data & computed & model) ------------------------ */
-  private playerId: string = 'aliplayer_' + Math.random() * 100000000000000000;
-  // private playerId: string = 'J_prismPlayer';
-  private scriptTagsStatus: number = 0;
+  public playerId: string = 'aliplayer_' + Math.random() * 100000000000000000;
+  // public playerId: string = 'J_prismPlayer';
+  public scriptTagsStatus: number = 0;
   public instance: any = null;
-  private danmukuList = [
+  public danmukuList = [
     {mode: 1, text: 'hello world', stime: 0, size: '25', color: '#0056dd'},
     {mode: 1, text: 'hello jean', stime: 10, size: '35', color: '#f00'},
     {mode: 1, text: 'who are you', stime: 30, size: '14', color: '#fff'},
@@ -121,7 +121,7 @@ export default class AliPlayer extends Vue {
   /* ------------------------ WATCH ------------------------ */
 
   /* ------------------------ METHODS ------------------------ */
-  private get_window_aliplayer() {
+  public get_window_aliplayer() {
     if ((window as any).Aliplayer !== undefined) {
       // 如果全局对象崔仔，说明编辑器代码已初始化完成，直接加载编辑器
       this.scriptTagsStatus = 2;
@@ -134,7 +134,7 @@ export default class AliPlayer extends Vue {
   /**
    * 插入播放节点
    */
-  private insertScriptTag() {
+  public insertScriptTag() {
     let playerScriptTag: any = document.getElementById('playerScriptTag');
 
     // 如果playerScriptTag不存在，则生成相关代码tag以加载代码
@@ -160,7 +160,7 @@ export default class AliPlayer extends Vue {
   /**
    * 初始化播放器
    */
-  private initAliplayer() {
+  public initAliplayer() {
     // scriptTagsStatus 为 2 的时候，说明两个必须引入的js文件都已经被引入，且加载完成
     if (this.scriptTagsStatus === 2 && this.instance === null) {
       this.$nextTick(() => {
@@ -245,37 +245,37 @@ export default class AliPlayer extends Vue {
   /**
    * 重播 视频
    */
-  private replay() {
+  public replay() {
     this.instance.replay();
   }
   /**
    * 获取当前的播放时刻，返回的单位为秒
    */
-  private seek(time: number) {
+  public seek(time: number) {
     this.instance.seek(time);
   }
   /**
    * 获取视频总时长，返回的单位为秒
    */
-  private getCurrentTime() {
+  public getCurrentTime() {
     this.instance.getCurrentTime();
   }
   /**
    * 获取视频总时长，返回的单位为秒
    */
-  private getDuration() {
+  public getDuration() {
     this.instance.getDuration();
   }
   /**
    * 获取当前的音量，返回值为0-1的实数ios和部分android会失效
    */
-  private getVolume() {
+  public getVolume() {
     this.instance.getVolume();
   }
   /**
    * 设置音量，vol为0-1的实数，ios和部分android会失效
    */
-  private setVolume() {
+  public setVolume() {
     this.instance.setVolume();
   }
   /**
@@ -283,13 +283,13 @@ export default class AliPlayer extends Vue {
    * @argument url 视频地址
    * @argument time 跳转到多少秒
    */
-  private loadByUrl(url: string, time: number) {
+  public loadByUrl(url: string, time: number) {
     this.instance.loadByUrl(url, time);
   }
   /**
    * 设置倍速播放移动端可能会失效，比如android 微信
    */
-  private setSpeed(speed: string) {
+  public setSpeed(speed: string) {
     this.instance.setSpeed(speed);
   }
   /**
@@ -297,7 +297,7 @@ export default class AliPlayer extends Vue {
    * @argument w 播放器宽度
    * @argument h 播放器高度
    */
-  private setPlayerSize(w: number, h: number) {
+  public setPlayerSize(w: number, h: number) {
     this.instance.setPlayerSize(w, h);
   }
   /**
@@ -305,17 +305,17 @@ export default class AliPlayer extends Vue {
    * @argument vid 视频id
    * @argument playauth 播放凭证
    */
-  private reloaduserPlayInfoAndVidRequestMts(vid: number, playauth: string) {
+  public reloaduserPlayInfoAndVidRequestMts(vid: number, playauth: string) {
     this.instance.reloaduserPlayInfoAndVidRequestMts(vid, playauth);
   }
   /**
    * 重新加载
    */
-  // private reloadPlayer() {
-  //   this.isReload = true;
-  //   this.initAliplayer();
-  //   this.isReload = false;
-  // }
+  public reloadPlayer() {
+    // this.isReload = true;
+    this.initAliplayer();
+    // this.isReload = false;
+  }
 }
 
 </script>
--- src/views/live/video-player.vue
+++ src/views/live/video-player.vue
@@ -45,13 +45,26 @@ export default class VideoPlayer extends Vue {
   /* ------------------------ WATCH ------------------------ */
 
   /* ------------------------ METHODS ------------------------ */
+  /**
+   * 播放
+   */
   private play() {
     const player = (this.$refs.player as any).instance;
     player && player.play();
   }
+  /**
+   * 暂停
+   */
   private pause() {
-      const player = (this.$refs.player as any).instance;
-      player && player.pause();
+    const player = (this.$refs.player as any).instance;
+    player && player.pause();
+  }
+  /**
+   * 重播
+   */
+  private replay() {
+    const player = (this.$refs.player as any).instance;
+    player && player.replay();
   }
 }
 
@@ -59,15 +72,14 @@ export default class VideoPlayer extends Vue {
 
 <template>
 <div class="module_living">
-  <!--  :play-style="playStyle" -->
-  <ali-player v-if="videopath !== ''" :source="videopath" ref="player"
-  :autoplay="islive" :is-live="islive" :use-h5-prism="type !== 'rtmp'"></ali-player>
-   <div style="margin-top:20px;">
-    <button @click="play">播放</button>
-    <button @click="pause">暂停</button>
-    <!-- <button @click="replay">重播</button>
-    <button @click="convert">切换</button> -->
+   <div class="play_optation_row">
+    <button class="op_btn" @click="play">播放</button>
+    <button class="op_btn" @click="pause">暂停</button>
+    <button class="op_btn" @click="replay">刷新视频</button>
+    <!-- <button @click="convert">切换</button> -->
   </div>
+  <ali-player v-if="videopath !== ''" :source="videopath" ref="player" :play-style="playStyle"
+  :autoplay="islive" :is-live="islive" :use-h5-prism="type !== 'rtmp'"></ali-player>
 
   <!-- <vue-aliplayer></vue-aliplayer> type === 'rtmp' -->
 </div>
@@ -79,5 +91,19 @@ export default class VideoPlayer extends Vue {
   margin 0 auto
   width 1024px
   height 576px
+  .play_optation_row
+    margin 8px 0
+    text-align right
+    .op_btn
+      margin-left 8px
+      cursor pointer
+      background rgba(255,255,255,0.3)
+      border 1px solid #727272
+      color #f0f0f0
+      border-radius 3px
+      font-size 12px
+      padding 4px 10px
+      &:hover
+        color #2196f3
 
 </style>
